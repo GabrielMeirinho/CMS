451b93961b2afe1770bc3b3934d525d116dbdf74
Gabriel Peixoto Meirinho <peixoto.meirinho@gmail.com>
2020-10-28 19:35:19 +0000

    adding project files

---
+++ cms/src/main/java/com/cms/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.cms.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/hello")
+	ResponseEntity<String> helloWorld(){
+		return ResponseEntity.status(HttpStatus.OK)
+		        .body("Hello World!");
+	}
+
+}
